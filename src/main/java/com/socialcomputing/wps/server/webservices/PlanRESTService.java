a661bc04ebb161111e46f07bddf4c421abe17e8f
Jonathan Dray <jonthan.dray@gmail.com>
2011-04-01 20:16:37 +0200

    Suppression de dépendances inutiles
    --HG--
    branch : feature/jersey

--- src/main/java/com/socialcomputing/wps/server/webservices/PlanRESTService.java
+++ src/main/java/com/socialcomputing/wps/server/webservices/PlanRESTService.java
@@ -1,6 +1,5 @@
 package com.socialcomputing.wps.server.webservices;
 
-import java.io.IOException;
 import java.rmi.RemoteException;
 import java.util.Hashtable;
 
@@ -13,12 +12,6 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.UriInfo;
 
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.map.deser.JsonNodeDeserializer;
-import org.codehaus.jackson.node.ObjectNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -81,7 +74,6 @@ public class PlanRESTService {
             PlanContainer planContainer = (PlanContainer) results.get("PLAN");
             ProtoPlan protoPlan = planContainer.m_protoPlan;
 
-            // TODO Construct a JSON formatted result from the BeanPlanMaker
             // TODO Get implementation by DI
             PlanJSONProvider planJSONProvider = new JacksonPlanJSONProvider();
             return planJSONProvider.planToString(protoPlan, planName);
