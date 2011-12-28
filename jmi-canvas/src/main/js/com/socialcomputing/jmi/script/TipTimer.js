13a14a5399e0532093a370d6afe836355a526233
Jonathan Dray <jonthan.dray@gmail.com>
2011-12-28 12:43:55 +0100

    Première version de portage de classes javascript : TipTimer

--- jmi-canvas/src/main/js/com/socialcomputing/jmi/script/TipTimer.js
+++ jmi-canvas/src/main/js/com/socialcomputing/jmi/script/TipTimer.js
@@ -1,97 +1,148 @@
-package com.socialcomputing.wps.script
-{
-	import flash.events.TimerEvent;
-	import flash.geom.Point;
-	import flash.geom.Rectangle;
-	import flash.utils.Timer;
+
+JMI.namespace("com.socialcomputing.wps.script.TipTimer") = (function() {
 		
-	public class TipTimer
-	{
-		private var timer:Timer;
-		private var start:int, duration:int;
-		private var _isInterrupted:Boolean;
+    // :Timer;
+    var timer,
+    
+        //:int
+	    start, 
+	    
+	    //:int
+	    duration,
+	    
+	    //:Boolean
+	    _isInterrupted,
+	    
+	    // :Plan
+	    _plan,
+	    
+	    // :ActiveZone
+	   _zone,
+	   
+	   // :Slice
+	   _slice,
+	
+	   // :Boolean
+	   _started,
+	
+	   // :String
+	   _key,
+	
+	   // :Rectangle
+	   _bounds;
+	
+	/**
+	 * @param plan //:Plan
+	 * @param zone //:ActiveZone
+	 * @param slice //:Slice
+	 * @param key //:String
+	 * @param start //:Number
+	 * @param duration // :int //default value -1
+	 */
+	this.prototype.init = function(plan, zone, slice, key, start, duration) {
+		this._isInterrupted = false;
 		
-		private var _plan:Plan;
-		private var _zone:ActiveZone;
-		private var _slice:Slice;
-		private var _started:Boolean;
-		private var _key:String;
-		private var _bounds:Rectangle;
+		this._plan = plan;
+		this._zone = zone;
+		this._slice = slice;
+		this._key = key;
+		this._bounds = new Rectangle(); 
+		this._started = false;
 		
-		public function TipTimer( plan:Plan, zone:ActiveZone, slice:Slice, key:String, start:Number, duration:int=-1)
-		{
-			this._isInterrupted = false;
-			
-			this._plan = plan;
-			this._zone = zone;
-			this._slice = slice;
-			this._key = key;
-			this._bounds = new Rectangle(); 
+		this.start = start;
+		this.duration = duration;
+		
+		// TODO: Portage : trouver un équivalent au timer
+		// timer = new Timer( start, 1);
+		// timer.addEventListener("timer", startHandler);
+		// timer.start();
+	};
+	
+	// :Slice 
+	this.prototype.getSlice = function() {
+		return _slice;
+	};
+
+    // :ActiveZone
+	this.prototype.getZone = function() {
+		return _zone;
+	};
+
+    // :Boolean
+	this.prototype.getStarted= function() {
+		return _started;
+	};
+	
+	// :void
+	this.prototype.interrupt = function() {
+		if (timer != null) {
+		    timer.stop();
+			clean(false);
+		}			
+	};
+	
+	/**
+	 * @param full //:Boolean 
+	 * @return //:void
+	 */
+	this.prototype.clean = function(full) {
+		if (this._started) {
 			this._started = false;
-			
-			this.start = start;
-			this.duration = duration;
-			timer = new Timer( start, 1);
-			timer.addEventListener("timer", startHandler);
-			timer.start();
-		}
-		
-		public function get slice():Slice
-		{
-			return _slice;
-		}
-
-		public function get zone():ActiveZone
-		{
-			return _zone;
-		}
-
-		public function get started():Boolean
-		{
-			return _started;
-		}
-		
-		public function interrupt():void {
-			if( timer != null) {
-				timer.stop();
-				clean( false);
-			}			
-		}
-		
-		public function clean( full:Boolean):void {
-			if( this._started) {
-				this._started = false;
-				if( _plan.m_applet.plan != null) {
-					// 
-					if( _bounds.width == 0 && _bounds.height == 0) {
-						// Asynchronous URL content loaded (bounds not set)
-						slice.setBounds( _plan.m_applet, _plan.m_applet.curDrawingSurface.graphics, zone.getParent(), zone, null, null, null, _bounds );
-					}
-					_plan.m_applet.renderShape( _plan.m_applet.restDrawingSurface, _bounds.width, _bounds.height, new Point( _bounds.x, _bounds.y));
-					if( full)
-						_plan.paintCurZone();
+			if ( _plan.m_applet.plan != null) {
+				 
+				if(_bounds.width == 0 && _bounds.height == 0) {
+					// Asynchronous URL content loaded (bounds not set)
+					slice.setBounds( _plan.m_applet, _plan.m_applet.curDrawingSurface.graphics, zone.getParent(), zone, null, null, null, _bounds );
 				}
-				delete _plan.m_tipTimers[_key];
+				_plan.m_applet.renderShape( _plan.m_applet.restDrawingSurface, _bounds.width, _bounds.height, new Point( _bounds.x, _bounds.y));
+				if( full) _plan.paintCurZone();
 			}
+			// TODO : portage : delete keyword in javascript ?
+			delete _plan.m_tipTimers[_key];
 		}
-		
-		public function startHandler(event:TimerEvent):void {
-			if( !this._started) {
-				this._started = true;
-				var pos:Point = _plan.m_applet.curPos;
-				slice.paint( _plan.m_applet, _plan.m_applet.curDrawingSurface, zone.getParent(), zone, null, pos, null );
-				slice.setBounds( _plan.m_applet, _plan.m_applet.curDrawingSurface.graphics, zone.getParent(), zone, null, pos, null, _bounds );
-				_plan.m_applet.renderShape( _plan.m_applet.curDrawingSurface, _bounds.width, _bounds.height, new Point( _bounds.x, _bounds.y));
-			}
-			if( duration != -1) {
-				timer = new Timer( duration, 1);
-				timer.addEventListener("timer", stopHandler);
-				timer.start();
-			}
+	};
+	
+	/**
+	 * @param event :TimerEvent
+	 * @return //:void
+	 */
+	this.prototype.startHandler = function(event) {
+		if( !this._started) {
+			this._started = true;
+			// :Point
+			var pos = _plan.m_applet.curPos;
+			slice.paint( _plan.m_applet, _plan.m_applet.curDrawingSurface, zone.getParent(), zone, null, pos, null );
+			slice.setBounds( _plan.m_applet, _plan.m_applet.curDrawingSurface.graphics, zone.getParent(), zone, null, pos, null, _bounds );
+			_plan.m_applet.renderShape( _plan.m_applet.curDrawingSurface, _bounds.width, _bounds.height, new Point( _bounds.x, _bounds.y));
 		}
-		
-		public function stopHandler(event:TimerEvent):void {
-			clean( true);
+		if( duration != -1) {
+		    // TODO portage : timer in javascript
+		    /*
+			timer = new Timer( duration, 1);
+			timer.addEventListener("timer", stopHandler);
+			timer.start();
+			*/
 		}
-	}
-}
+	};
+	
+
+    /**
+     * @param event :TimerEvent
+     * @return //:void
+     */
+    this.prototype.stopHandler = function(event) {
+        clean(true);
+    };
+    
+    // Public API
+    return  {
+        init: init,
+        getSlice: getSlice,
+        getZone: getZone,
+        getStarted: getStarted,
+        interrupt: interrupt,
+        clean: clean,
+        startHandler: startHandler,
+        stopHandler: stopHandler,
+    };
+}());
