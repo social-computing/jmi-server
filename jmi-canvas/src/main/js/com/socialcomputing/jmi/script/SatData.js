0fd3d7fc3525162f08fa5ce14be57ca7007d4c02
Jonathan Dray <jonthan.dray@gmail.com>
2011-12-28 14:45:32 +0100

    Premiere version du portage de la classe SatData

--- jmi-canvas/src/main/js/com/socialcomputing/jmi/script/SatData.js
+++ jmi-canvas/src/main/js/com/socialcomputing/jmi/script/SatData.js
@@ -1,67 +1,55 @@
-package com.socialcomputing.wps.script  {
- 
+/**
+ * <p>Title: SatData</p>
+ * <p>Description: Bufferized satellites data of the swatchs.<vr>
+ * Each satellite have its SatData buffer.
+ * So 2 arrays are created, one for each swatch of an ActiveZone.
+ * This avoid CPU overhead when accessing to props.
+ * Initally the goal was to store much more properties and graphical values.
+ * As the Applet is not that slow, optimization was quickly stoped...</p>
+ * <p>Copyright: Copyright (c) 2001-2003</p>
+ * 
+ * @author flugue@mapstan.com
+ * @version 1.0
+ */
+JMI.namespace("com.socialcomputing.wps.script.SatData") = (function() {
+
+    // :int;
+    var _m_flags, 
+         //:Boolean
+        _m_isVisible;
+
     /**
-     * <p>Title: SatData</p>
-     * <p>Description: Bufferized satellites data of the swatchs.<vr>
-     * Each satellite have its SatData buffer.
-     * So 2 arrays are created, one for each swatch of an ActiveZone.
-     * This avoid CPU overhead when accessing to props.
-     * Initally the goal was to store much more properties and graphical values.
-     * As the Applet is not that slow, optimization was quickly stoped...</p>
-     * <p>Copyright: Copyright (c) 2001-2003</p>
-     * <p>Company: MapStan (Voyez Vous)</p>
-     * @author flugue@mapstan.com
-     * @version 1.0
+     * Flags of the Satellite after they were retieved using properties.
      */
-    public class SatData
-    {
-        private var _m_flags:int;
-        
-        private var _m_isVisible:Boolean;
-        
-        /*public String getXML()
-        {
-        String result = new String();
-        
-        result = "<flag visible='"+(m_isVisible==true?"true":"false")+"'>\n";
-        result += m_flags;
-        result = "</flag>\n";
-        
-        return result;
-        }	*/
+    // public function get m_flags():int
+    this.prototype.getFlags = function() {
+        return _m_flags;
+    };
 
-        /**
-         * Flags of the Satellite after they were retieved using properties.
-         */
-        public function get m_flags():int
-        {
-            return _m_flags;
-        }
+    // public function set m_flags(value:int):void
+    this.prototype.setFlags = function(value) {
+        _m_flags = value;
+    };
 
-        /**
-         * @private
-         */
-        public function set m_flags(value:int):void
-        {
-            _m_flags = value;
-        }
-
-        /**
-         * True if this Satellite is visible.
-         */
-        public function get m_isVisible():Boolean
-        {
-            return _m_isVisible;
-        }
-
-        /**
-         * @private
-         */
-        public function set m_isVisible(value:Boolean):void
-        {
-            _m_isVisible = value;
-        }
+    /**
+     * True if this Satellite is visible.
+     */
+    // public function get m_isVisible():Boolean
+    this.prototype.isVisible = function() {
+        return _m_isVisible;
+    };
 
 
-    }
-}
+    // public function set m_isVisible(value:Boolean):void
+    this.prototype.setVisible = function(value) {
+        _m_isVisible = value;
+    };
+    
+    // Public API
+    return  {
+        getFlags: getFlags,
+        setFlags: setFlags,
+        isVisible: isVisible,
+        setVisible: setVisible
+    };
+}());
