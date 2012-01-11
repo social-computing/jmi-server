491dfefc88c0c112410fe8e9d67d22cf354ee7a6
Jonathan Dray <jonthan.dray@gmail.com>
2012-01-11 18:44:49 +0100

    Maj de la classe Dimension, variables d'instance et méthode publique statique.

--- jmi-canvas/src/main/js/script/Dimension.js
+++ jmi-canvas/src/main/js/script/Dimension.js
@@ -2,38 +2,38 @@ JMI.namespace("script.Dimension");
 
 JMI.script.Dimension = (function() {
 	
-	var width, height;
-	
 	var Dimension = function(w, h) {
-		width = w;
-		height = h;
+		this._width = w;
+		this._height = h;
 	};
 	
     Dimension.prototype = {
         contructor: JMI.script.Dimension,
 		
-		resize: function( d) {
-			return new JMI.script.Dimension(Math.max(this.width, d.width), Math.max(this.height, d.height));
-		}
+		resize: function(d) {
+			return new JMI.script.Dimension(Math.max(this._width, d._width), Math.max(this._height, d._height));
+		},
+		
+		/**
+         * Create a <code>Rectangle</code> instance with height and width of the current Dimension
+         */
+		toRectangle: function(){
+            return new JMI.script.Rectangle(0, 0, this._width, this._height);
+        }
 	};
 	
 	return Dimension;
 }());
 
-// static
-/**
- * Create a <code>Rectangle</code> instance with height and width of the current Dimension
- */
-com.socialcomputing.jmi.script.Dimension.toRectangle = function(){
-	return new JMI.script.Rectangle(0, 0, this._width, this._height);
-}
 
+// Public static methods
 /**
  *  Construct a new Dimension Object from a <code>Rectangle</code>
  * 	It is done this way instead of a specific constructor because of ActionScript3
- * contructor overload limitations : on class can only have one constructor signature. 
+ * contructor overload limitations : on class can only have one constructor signature.
+ *  
  */
-com.socialcomputing.jmi.script.Dimension.fromRectangle = function( rect) {
+// TODO : Try to handle this with the class constructor
+JMI.script.Dimension.fromRectangle = function(rect) {
 	return new JMI.script.Dimension(rect.width, rect.height);
-}
-		
+};
