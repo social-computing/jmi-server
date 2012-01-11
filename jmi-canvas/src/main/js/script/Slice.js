7e9c186e9c10be1b7bc491856d319a91deb61899
Jonathan Dray <jonthan.dray@gmail.com>
2012-01-11 15:34:54 +0100

    Portage classe Slice : premier niveau de portage, maj de la syntaxe de la classe

--- jmi-canvas/src/main/js/script/Slice.js
+++ jmi-canvas/src/main/js/script/Slice.js
@@ -1,3 +1,5 @@
+JMI.namespace("script.Slice");
+
 /*
  * <p>Title: Slice</p>
  * <p>Description: An elementary slice to fill with graphics.<br>
@@ -7,247 +9,238 @@
  * @author flugue@mapstan.com
  * @version 1.0
  */
-JMI.namespace("com.socialcomputing.jmi.script.Slice") = (function() {
-/**
- * A buffer to store the HTMLText associated with this if it has one.
- */
-	var m_htmlTxt, //:HTMLText;
-	Constr;
+JMI.script.Slice =  (function() {
+    
+	var Slice = function() {
+	    /**
+         * A buffer to store the HTMLText associated with this if it has one.
+         * //:HTMLText;
+         */
+	    this._htmlTxt = null;
+	};
 	
-	Constr = function() {
-	}
-	Constr.prototype = {
-		constructor: com.socialcomputing.jmi.script.Slice,
-		version: "2.0"
-	}
-	return Constr;
+	Slice.prototype = {
+		constructor: JMI.script.Slice,
+		
+		/*
+         * Draws this slice on a Graphics.
+         * It's position and size is evaluated by its parent satellite and transfo.
+         * The inner parts (when they exists) are drawn with respect to the following order:
+         * <ul>
+         * <li>IN_COL_VAL : The background of the shape.</li>
+         * <li>OUT_COL_VAL : The outside of the shape.</li>
+         * <li>IMAGE_VAL : The bitmap (icon).</li>
+         * <li>TEXT_VAL : The text, standard or HTML.</li>
+         * </ul>
+         * @param applet        The Applet that owns this.
+         * @param s             A sprite to draw this in.
+         * @param supZone       The parent zone of this zone.
+         * @param zone          The zone that holds the properties used by this slice.
+         * @param satShp        The shape of this Slice
+         * @param satCtr        This slice center.
+         * @param supCtr        This parent satellite center.
+         */
+        paint: function(applet, s, supZone, zone, satShp, satCtr, supCtr) {
+            var text = getText( TEXT_VAL, zone._props );
+            
+            var transfo = getTransfo( TRANSFO_VAL, zone._props );
+            
+            // Draw a satellite with primitives
+            if(isDefined(IN_COL_VAL) || isDefined(OUT_COL_VAL)) {
+                satShp.paint(s, supZone, zone, this, transfo, satCtr );
+            }
+            
+            // Draw a satellite's image it is set
+            if(isDefined(IMAGE_VAL)) {
+                var imageNam = parseString(IMAGE_VAL, zone._props )[0];
+                if (imageNam != null) {
+                    satShp.drawImage(applet, s, supZone, imageNam, transfo, satCtr);
+                }
+            }
+            
+            if(text != null) {
+                if (HTMLText.isEnabled(text.getFlags(zone._props), HTMLText.URL_BIT)) {
+                    m_htmlTxt = null;
+                    var textUrls  = text.parseString(HTMLText.TEXT_VAL, zone._props);
+                    var t , hTxt = "";
+                    var i = 0;
+                    // TODO portage
+                    /*  var textLoader:URLLoader = new URLLoader();
+                    textLoader.addEventListener(Event.COMPLETE, function(e:Event):void {
+                        t = textLoader.data as String;
+                        if( t!= null) 
+                            hTxt = hTxt + t;
+                        ++i;
+                        if ( hTxt.length > 0 && i == textUrls.length)
+                        {
+                            m_htmlTxt = new HTMLText();
+                            m_htmlTxt.m_text = hTxt;
+                            m_htmlTxt.init( text, zone);
+                            m_htmlTxt.updateBounds( applet);
+                            supCtr  = supZone.m_restSwh.m_satellites[0].m_shape.getCenter( supZone );
+                            m_htmlTxt.setTextBnds( applet.size, getFlags( zone.m_props), zone.m_flags ,transfo, satCtr, supCtr );
+                            m_htmlTxt.drawText( s, applet.size, text.getFlags( zone.m_props )>> 16);//HTMLText.SOUTH_WEST );
+                            applet.renderShape( s, m_htmlTxt.m_bounds.width, m_htmlTxt.m_bounds.height, new Point(m_htmlTxt.m_bounds.x, m_htmlTxt.m_bounds.y));
+                        }
+                    });
+                    for each ( var url:String in textUrls)
+                    {
+                        var textReq:URLRequest = new URLRequest(url);
+                        textLoader.load(textReq);
+                    }*/
+                }
+                else {
+                    supCtr = supZone._restSwh._satellites[0]._shape.getCenter(supZone);
+                    var htmlTxt = text.getHText(applet, s, zone, transfo, satCtr, supCtr, text);
+                    
+                    if (htmlTxt != null && htmlTxt._text.length > 0) {
+                        htmlTxt.drawText2(s, applet.size);
+                        zone.m_datas[text] = htmlTxt;
+                    }
+                }
+            }
+        },
+        
+        /**
+         * Return wether a point is inside this slice
+         * 
+         * If there is a border or a background, tests if the point is inside the shape.
+         * TODO : Else, if there is a Text or HTMLText, tests if the point is inside the text bounds. 
+         * TODO : The image are not considered because of the complexity (retrieving size), but should...
+         * 
+         * @param applet        The PlanComponent that owns this.
+         * @param g             A graphics to get the FontMetrics used by this.
+         * @param supZone       The parent of zone or null if it have none.
+         * @param zone          The zone that holds the properties used by this slice.
+         * @param satShp        This slice shape, get from its satellite.
+         * @param satCtr        This slice center, get from its satellite.
+         * @param supCtr        This parent satellite center.
+         * @param pos           A point position to test.
+         * 
+         * @return              True if the cursor's position is inside this slice, false otherwise
+         */
+        contains: function(planComponent, g, supZone, zone, satShp, satCtr, supCtr, pos){
+            var transfo = getTransfo(TRANSFO_VAL, zone._props);
+            
+            if(supZone == null) supZone = zone;
+            
+            if((isDefined(IN_COL_VAL ) || isDefined(OUT_COL_VAL)) &&
+                satShp.contains(g, supZone, transfo, satCtr, pos)) {
+                return true;
+            }
+            
+            var text = getText(TEXT_VAL, zone._props);
+            if (text != null) {
+                // TODO : null à remplacer
+                var htmlTxt = text.getHText(planComponent, null, zone, transfo, satCtr, supCtr, text);
+                return htmlTxt != null ? htmlTxt._bounds.contains(pos._x, pos._y): false;
+            }
+            return false;
+        },
+        
+        /*
+         * Sets this bounds by updating an already created Rectangle.
+         * If there is a border or a background, the bounds of the shape are considered.
+         * If there is a Text or HTMLText, the bounds of the text are considered.
+         * The image bounds are not considered because of the complexity (retrieving size), but they should...
+         * 
+         * @param applet        The Applet that owns this.
+         * @param g             A graphics to get the FontMetrics used by this.
+         * @param supZone       The parent of zone or null if it have none.
+         * @param zone          The zone that holds the properties used by this slice.
+         * @param satShp        This slice shape, get from its satellite.
+         * @param satCtr        This slice center, get from its satellite.
+         * @param supCtr        This parent satellite center.
+         * @param bounds        A Rectangle to merge with this bounds.
+         */
+        setBounds: function(applet, g, supZone, zone, satShp, satCtr, supCtr, bounds) {
+            var transfo = getTransfo(TRANSFO_VAL, zone._props);
+            
+            if (supZone == null) supZone = zone;
+            
+            try {
+                if (isDefined(IN_COL_VAL) || isDefined(OUT_COL_VAL)) {
+                    satShp.setBounds(g, supZone, transfo, satCtr, bounds);
+                }
+            }
+            catch (e) {
+                var errorMessage = "getCenter supZone=" + supZone;
+                if (supZone != null) {
+                    var points = satShp.getValue(ShapeX.POLYGON_VAL, supZone._props);
+                    errorMessage += " zName=" + supZone._props[ "NAME" ] + " pKey=" + satShp._containers[ShapeX.POLYGON_VAL]._value + " pnts=" + points + " p[0]=" + points[0];
+                }
+                throw(new Error(errorMessage));
+            }
+        
+            var text = getText( TEXT_VAL, zone._props);
+            if (text != null) {
+                if (HTMLText.isEnabled(text.getFlags(zone._props), HTMLText.URL_BIT)) {
+                    if (this._htmlTxt != null) bounds.copy(this._htmlTxt._bounds);
+                }
+                else {
+                    supCtr = supZone._restSwh._satellites[0]._shape.getCenter(supZone);
+                    var htmlTxt;
+                    
+                    // TODO null à remplacer par Sprite
+                    // TODO voir à quel propriété se réfère bounds
+                    htmlTxt = text.getHText(applet, null, zone, transfo, satCtr, supCtr, text);
+                    if (htmlTxt != null && htmlTxt._text.length > 0) {
+                        bounds.merge(htmlTxt._bounds);
+                    }
+                }
+            }
+        }
+
+	};
+	
+	return Slice;
 }());
 
 
+// Constants
 /**
  * Index of the Transfo prop in VContainer table.
  * If it doesn't exists this Slice will have the same shape as its satellite.
  */
-com.socialcomputing.jmi.script.Slice.TRANSFO_VAL = 1;
+JMI.script.Slice.TRANSFO_VAL = 1;
 
 /**
  * Index of the inside Color prop in VContainer table.
  */
-com.socialcomputing.jmi.script.Slice.IN_COL_VAL = 2;
+JMI.script.Slice.IN_COL_VAL = 2;
 
 /**
  * Index of the border Color prop in VContainer table.
  */
-com.socialcomputing.jmi.script.Slice.OUT_COL_VAL = 3;
+JMI.script.Slice.OUT_COL_VAL = 3;
 
 /**
  * Index of the image URL prop in VContainer table.
  */
-com.socialcomputing.jmi.script.Slice.IMAGE_VAL = 4;
+JMI.script.Slice.IMAGE_VAL = 4;
 
 /**
  * Index of the text (HTML or not) prop in VContainer table.
  */
-com.socialcomputing.jmi.script.Slice.TEXT_VAL = 5;
+JMI.script.Slice.TEXT_VAL = 5;
 
 /**
  * Index of the text (HTML or not) prop in VContainer table.
  */
-com.socialcomputing.jmi.script.Slice.ALPHA_VAL = 6;
+JMI.script.Slice.ALPHA_VAL = 6;
 
 /**
  * Index of the delay (ms) prop for a tooltip Slice.
  */
-com.socialcomputing.jmi.script.Slice.DELAY_VAL = 7;
+JMI.script.Slice.DELAY_VAL = 7;
 
 /**
  * Index of the length (ms) prop for a tooltip Slice.
  */
-com.socialcomputing.jmi.script.Slice.LENGTH_VAL = 8;
+JMI.script.Slice.LENGTH_VAL = 8;
 
 /**
  * True if this Slice is visible.
  * Probably a useless bit...
  */
-com.socialcomputing.jmi.script.Slice.VISIBLE_BIT = 0x01;
-
-
-/*
- * Draws this slice on a Graphics.
- * It's position and size is evaluated by its parent satellite and transfo.
- * The inner parts (when they exists) are drawn with respect to the following order:
- * <ul>
- * <li>IN_COL_VAL : The background of the shape.</li>
- * <li>OUT_COL_VAL : The outside of the shape.</li>
- * <li>IMAGE_VAL : The bitmap (icon).</li>
- * <li>TEXT_VAL : The text, standard or HTML.</li>
- * </ul>
- * @param applet		The Applet that owns this.
- * @param g				A graphics to draw this in.
- * @param supZone		The parent zone of this zone.
- * @param zone			The zone that holds the properties used by this slice.
- * @param satShp		The shape of this Slice
- * @param satCtr		This slice center.
- * @param supCtr		This parent satellite center.
- * @throws UnsupportedEncodingException 
- */
-com.socialcomputing.jmi.script.Slice.prototype.paint = function(applet, s, supZone, zone, satShp, satCtr, supCtr) {
-	var text = getText( TEXT_VAL, zone.m_props );
-    
-    var transfo = getTransfo( TRANSFO_VAL, zone.m_props );
-    
-	// Draw a satellite with primitives
-    if(isDefined(IN_COL_VAL) || isDefined(OUT_COL_VAL)) {
-        satShp.paint( s, supZone, zone, this, transfo, satCtr );
-    }
-    
-	// Draw a satellite's image it is set
-    if(isDefined(IMAGE_VAL)) {
-		var imageNam = parseString(IMAGE_VAL, zone.m_props )[0];
-        if (imageNam != null) {
-            satShp.drawImage(applet, s, supZone, imageNam, transfo, satCtr);
-        }
-    }
-    
-    if(text != null) {
-        if ( HTMLText.isEnabled( text.getFlags( zone.m_props ), HTMLText.URL_BIT ))
-        {
-			m_htmlTxt = null;
-            var textUrls  = text.parseString( HTMLText.TEXT_VAL, zone.m_props );
-            var t , hTxt = "";
-            var i = 0;
-            // TODO portage
-		/*	var textLoader:URLLoader = new URLLoader();
-			textLoader.addEventListener(Event.COMPLETE, function(e:Event):void {
-				t = textLoader.data as String;
-				if( t!= null) 
-					hTxt = hTxt + t;
-				++i;
-				if ( hTxt.length > 0 && i == textUrls.length)
-				{
-					m_htmlTxt = new HTMLText();
-					m_htmlTxt.m_text = hTxt;
-					m_htmlTxt.init( text, zone);
-					m_htmlTxt.updateBounds( applet);
-					supCtr	= supZone.m_restSwh.m_satellites[0].m_shape.getCenter( supZone );
-					m_htmlTxt.setTextBnds( applet.size, getFlags( zone.m_props), zone.m_flags ,transfo, satCtr, supCtr );
-					m_htmlTxt.drawText( s, applet.size, text.getFlags( zone.m_props )>> 16);//HTMLText.SOUTH_WEST );
-					applet.renderShape( s, m_htmlTxt.m_bounds.width, m_htmlTxt.m_bounds.height, new Point(m_htmlTxt.m_bounds.x, m_htmlTxt.m_bounds.y));
-				}
-			});
-			for each ( var url:String in textUrls)
-			{
-				var textReq:URLRequest = new URLRequest(url);
-				textLoader.load(textReq);
-			}*/
-        }
-        else
-        {
-            supCtr	= supZone.m_restSwh.m_satellites[0].m_shape.getCenter( supZone );
-            var htmlTxt = text.getHText( applet, s, zone, transfo, satCtr, supCtr, text );
-            
-            if ( htmlTxt != null && htmlTxt.m_text.length > 0)
-            {
-                htmlTxt.drawText2( s, applet.size);
-                zone.m_datas[text] = htmlTxt;
-            }
-        }
-    }
-}
-
-
-/**
- * Return wether a point is inside this slice
- * 
- * If there is a border or a background, tests if the point is inside the shape.
- * TODO : Else, if there is a Text or HTMLText, tests if the point is inside the text bounds. 
- * TODO : The image are not considered because of the complexity (retrieving size), but should...
- * 
- * @param applet		The PlanComponent that owns this.
- * @param g				A graphics to get the FontMetrics used by this.
- * @param supZone		The parent of zone or null if it have none.
- * @param zone			The zone that holds the properties used by this slice.
- * @param satShp		This slice shape, get from its satellite.
- * @param satCtr		This slice center, get from its satellite.
- * @param supCtr		This parent satellite center.
- * @param pos			A point position to test.
- * 
- * @return				True if the cursor's position is inside this slice, false otherwise
- */
-com.socialcomputing.jmi.script.Slice.prototype.contains = function(planComponent, g, supZone, zone, satShp, satCtr, supCtr, pos){
-	var transfo = getTransfo(TRANSFO_VAL, zone.m_props);
-    
-    if(supZone == null)	supZone = zone;
-    
-    if((isDefined(IN_COL_VAL ) || isDefined(OUT_COL_VAL)) &&
-		satShp.contains(g, supZone, transfo, satCtr, pos)) {
-        return true;
-    }
-    
-    var text = getText( TEXT_VAL, zone.m_props);
-    if ( text != null )
-    {
-		// TODIO null à remplacer
-        var htmlTxt = text.getHText( planComponent, null, zone, transfo, satCtr, supCtr, text);
-        return htmlTxt != null ? htmlTxt.m_bounds.contains( pos.x, pos.y ): false;
-    }
-    return false;
-}
-
-
-/*
- * Sets this bounds by updating an already created Rectangle.
- * If there is a border or a background, the bounds of the shape are considered.
- * If there is a Text or HTMLText, the bounds of the text are considered.
- * The image bounds are not considered because of the complexity (retrieving size), but they should...
- * @param applet		The Applet that owns this.
- * @param g				A graphics to get the FontMetrics used by this.
- * @param supZone		The parent of zone or null if it have none.
- * @param zone			The zone that holds the properties used by this slice.
- * @param satShp		This slice shape, get from its satellite.
- * @param satCtr		This slice center, get from its satellite.
- * @param supCtr		This parent satellite center.
- * @param bounds		A Rectangle to merge with this bounds.
- */
-com.socialcomputing.jmi.script.Slice.prototype.setBounds = function(applet, g, supZone, zone, satShp, satCtr, supCtr, bounds) {
-    var transfo = getTransfo(TRANSFO_VAL, zone.m_props);
-    
-    if (supZone == null) supZone = zone;
-    
-    try {
-        if (isDefined(IN_COL_VAL) || isDefined(OUT_COL_VAL)) {
-            satShp.setBounds(g, supZone, transfo, satCtr, bounds);
-        }
-    }
-    catch (e) {
-        var errorMessage = "getCenter supZone=" + supZone;
-        if ( supZone != null )
-        {
-            var points = satShp.getValue( ShapeX.POLYGON_VAL, supZone.m_props );
-			errorMessage += " zName=" + supZone.m_props[ "NAME" ] + " pKey=" + satShp.m_containers[ShapeX.POLYGON_VAL].m_value + " pnts=" + points + " p[0]=" + points[0];
-        }
-        throw(new Error(errorMessage));
-    }
-
-	var text = getText( TEXT_VAL, zone.m_props);
-    if ( text != null )
-    {
-        if ( HTMLText.isEnabled( text.getFlags( zone.m_props), HTMLText.URL_BIT ))
-        {
-            if ( m_htmlTxt != null )
-				bounds.copy( m_htmlTxt.m_bounds );
-        }
-        else
-        {
-            supCtr	= supZone.m_restSwh.m_satellites[0].m_shape.getCenter( supZone );
-            var htmlTxt;
-            
-			// TODO null à remplacer par Sprite
-                htmlTxt = text.getHText( applet, null, zone, transfo, satCtr, supCtr, text);
-                if ( htmlTxt != null && htmlTxt.m_text.length > 0)
-                {
-                    bounds.merge( htmlTxt.m_bounds );
-                }
-        }
-    }
-}
+JMI.script.Slice.VISIBLE_BIT = 0x01;
