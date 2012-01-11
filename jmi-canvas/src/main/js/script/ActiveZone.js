49dd9e9f47cb31fbb785c31e4d2ff596ca6ba213
Jonathan Dray <jonthan.dray@gmail.com>
2012-01-11 16:26:27 +0100

    Premier niveau de portage des classes ActiveZone et BagZone (Syntaxe des classes)

--- jmi-canvas/src/main/js/script/ActiveZone.js
+++ jmi-canvas/src/main/js/script/ActiveZone.js
@@ -1,3 +1,5 @@
+JMI.namespace("script.ActiveZone");
+
 /*
  * <p>Title: ActiveZone</p>
  * <p>Description: A graphical zone holding properties.<br>
@@ -5,124 +7,128 @@
  * through BagZone it can also be a parent zone (superZone).
  * This kind of zone doesn't contains any graphical informations.</p>
  */
-JMI.namespace("com.socialcomputing.jmi.script.ActiveZone") = (function() {
-	// Private methods
+JMI.script.ActiveZone = (function() {
 	
-    	// Swatch used to render this zone at rest.
-    var m_restSwh = com.socialcomputing.jmi.script.Swatch,
-     	//Swatch used to render this zone when it is current (hovered).
-    	m_curSwh = com.socialcomputing.jmi.script.Swatch,
+	var ActiveZone = function() {
+	    
+	    // Swatch used to render this zone at rest.
+	    // :jmi.script.Swatch
+        this._restSwh = null;
+        
+        // Swatch used to render this zone when it is current (hovered).
+        // :jmi.script.Swatch
+        this._curSwh = null;
+        
         // Flags holding the previously defined bits (XXX_BIT).
-        m_flags,
-		m_props, //Array
+        this._flags = null;
+        this._props = null; //Array
+        
         /*
          * Bounding-Box of this zone including its subZones.
          * The BBox is the union of the rest and current swatch BBoxs and a small margin.
+         * :jmi.script.Rectangle
          */
-        m_bounds = com.socialcomputing.jmi.script.Rectangle, 
-         //Flag indicating which of the 32 possible selections are active for this zone.
-        m_selection,
-         //Parent of this zone if it is clusterized or null if this is already a BagZone.
-        m_parent, //:ActiveZone;
+        this._bounds = null; 
+        
+        //Flag indicating which of the 32 possible selections are active for this zone.
+        this._selection = null;
+        
+        //Parent of this zone if it is clusterized or null if this is already a BagZone.
+        //:ActiveZone;
+        this._parent = null; 
+        
         /*
          * Fast graphical data lookup for the rest Swatch Satellites.
          * Not used enough, could improve the performance if more was stored here...
+         * :Vector.<SatData>;
          */
-        m_restData, //:Vector.<SatData>;
+        this._restData = null; 
+        
         /*
          * Fast graphical data lookup for the current Swatch Satellites.
          * Not used enough, could improve the performance if more was stored here...
+         * :Vector.<SatData>;
          */
-        m_curData, //:Vector.<SatData>;
-         // HTMLText dictionary to avoid unnecessary calcs.
-        m_datas, //:Dictionary;
-
-		Constr = function() {
-		}
-		Constr.prototype = {
-			constructor: com.socialcomputing.jmi.script.ActiveZone,
-			version: "2.0"
-		}
-		return Constr;
+        this._curData = null; 
+        
+        // HTMLText dictionary to avoid unnecessary calcs.
+        //:Dictionary;
+        this._datas = null; //:Dictionary;
+	};
+	
+	ActiveZone.prototype = {
+		constructor: JMI.script.ActiveZone,
+		
+		/*
+         * Perform basic buffer initialization to enhance real time performance.
+         * This include transforming selection prop to an int flag,
+         * copying Env props reference in this prop table and
+         * initializing satellite data for both swatchs.
+         * TODO : Remove graphics parameter : unused
+         * 
+         * @param applet    WPSApplet owning this.
+         * @param g         A graphics compatible with the one that will be used for painting.
+         * @param isFirst   True if init called for the first time.
+         */
+        init: function (applet, s, isFirst) {
+            // One time init
+            if (isFirst) {
+                var sel = this._props["SELECTION"];
+                if (sel != null) {
+                    this._selection = sel;
+                }
+                
+                // Quick access to Env props
+                this._props["ENV"] = applet.env._props;
+                this.datas = {};
+            }
+            
+            // TODO : portage instanceof et heritage
+            var isSuper = this instanceof JMI.script.BagZone;
+            
+            this._restData = this._restSwh.evalSatData(applet, this, isSuper);
+            
+            if (this._curSwh != null) {
+                this._curData = this._curSwh.evalSatData(applet, this, isSuper);
+            }
+        },
+        
+        /*
+         * Draw this zone on a specified Graphics using the rest or cur swatch.
+         * 
+         * @param applet    WPSApplet owning this zone.
+         * @param s         A sprite on which this must be painted.
+         * @param isCur     True if this is the current zone (hovered) of the plan.
+         * @param isFront   True if this zone appears on top of ghosted zones.
+         * @param showTyp   The type of Satellite to show (SEL, TIP, BASE, ALL). See Satellite.XXXX_TYP.
+         * @param showLinks True if we only wants to paint links.
+         */
+        paint: function(applet, s, isCur, isFront, showTyp, showLinks) {
+            if ((this._flags & INVISIBLE_BIT) != 0) return;
+            var swatch = isCur ? this._curSwh : this._restSwh;
+            
+            swatch.paint(applet, s, this, isCur, isFront, showTyp, showLinks);
+        },
+        
+        /*
+         * Get this parent zone if it exists.
+         * 
+         * @return  The BagZone holding this or null if this is a BagZone.
+         */
+        getParent: function () {
+            return this._parent == null ? this : this._parent;
+        }
+	};
+	
+	return ActiveZone;
 }());
 
 // Constantes
 // Bit indicating that subnodes of this are located on one side.
-com.socialcomputing.jmi.script.ActiveZone.SIDE_BIT = 0x04;
-// Bit indicating that subnodes are located on the left side.
-com.socialcomputing.jmi.script.ActiveZone.LEFT_BIT = 0x08;
-// Bit indicating invisibility.
-com.socialcomputing.jmi.script.ActiveZone.INVISIBLE_BIT = 0x10;
+JMI.script.ActiveZone.SIDE_BIT = 0x04;
 
-com.socialcomputing.jmi.script.ActiveZone.prototype.getRestSwatch = function () {
-    return m_restSwh;
-};
-com.socialcomputing.jmi.script.ActiveZone.prototype.getCurSwatch = function () {
-    return m_curSwh;
-};
-/*
- * Sets the two swatchs of this zone.
- * This is used in PlanGenerator to setup this zone's swatchs.
- * @param restSwh   Swatch used to render this when it is at rest.
- * @param curSwh    Swatch used to render this when it is hovered.
- */
-com.socialcomputing.jmi.script.ActiveZone.prototype.setSwatchs = function ( restSwh, curSwh) {
-    m_restSwh   = restSwh;
-    m_curSwh    = curSwh;
-};
-/*
- * Perform basic buffer initialization to enhance real time performance.
- * This include transforming selection prop to an int flag,
- * copying Env props reference in this prop table and
- * initializing satellite data for both swatchs.
- * TODO : Remove graphics parameter : unused
- * 
- * @param applet    WPSApplet owning this.
- * @param g         A graphics compatible with the one that will be used for painting.
- * @param isFirst   True if init called for the first time.
- */
-com.socialcomputing.jmi.script.ActiveZone.prototype.init = function (applet, s, isFirst) {
-    if ( isFirst )  // One time init
-    {
-        var sel = m_props["SELECTION"];
-        if ( sel != null )
-        {
-            m_selection = sel;
-        }
-        
-        // Quick access to Env props
-		m_props["ENV"] = applet.env.m_props;
-        m_datas = new Object();
-    }
-    
-    var isSuper = this instanceof com.socialcomputing.jmi.script.BagZone;
-    
-    m_restData = m_restSwh.evalSatData( applet, this, isSuper);
-    
-    if ( m_curSwh != null )
-    {
-        m_curData   = m_curSwh.evalSatData( applet, this, isSuper );
-    }
-};
-/*
- * Draw this zone on a specified Graphics using the rest or cur swatch.
- * @param applet    WPSApplet owning this zone.
- * @param g         A Graphics on which this must be painted.
- * @param isCur     True if this is the current zone (hovered) of the plan.
- * @param isFront   True if this zone appears on top of ghosted zones.
- * @param showTyp   The type of Satellite to show (SEL, TIP, BASE, ALL). See Satellite.XXXX_TYP.
- * @param showLinks True if we only wants to paint links.
- */
-com.socialcomputing.jmi.script.ActiveZone.prototype.paint = function (applet, s, isCur, isFront, showTyp, showLinks) {
-    if( (m_flags & INVISIBLE_BIT) != 0) return;
-    var swatch = isCur ? m_curSwh : m_restSwh;
-    
-    swatch.paint( applet, s, this, isCur, isFront, showTyp, showLinks );
-};
- /*
- * Get this parent zone if it exists.
- * @return	The BagZone holding this or null if this is a BagZone.
- */
-com.socialcomputing.jmi.script.ActiveZone.prototype.getParent = function () {
-    return m_parent == null ? this : m_parent;
-};
+// Bit indicating that subnodes are located on the left side.
+JMI.script.ActiveZone.LEFT_BIT = 0x08;
+
+// Bit indicating invisibility.
+JMI.script.ActiveZone.INVISIBLE_BIT = 0x10;
--- jmi-canvas/src/main/js/script/BagZone.js
+++ jmi-canvas/src/main/js/script/BagZone.js
@@ -1,3 +1,5 @@
+JMI.namespace("script.BagZone");
+
 /*
  * <p>Title: BagZone</p>
  * <p>Description: A graphical Place holding properties.<br>
@@ -7,171 +9,158 @@
  * @author flugue@mapstan.com
  * @version 1.0
  */
-JMI.namespace("com.socialcomputing.jmi.script.BagZone") = (function() {
-	 // Clusterized subZones table.
-	var m_subZones, //:Array = null;
-
-	/*
-	 * Initial angular direction of Satellites.
-	 * It change depending on the distance to window borders.
-	 * The main zone will start at this angle. Evaluated during init.
-	 */
-	_m_dir,
-
-	/*
-	 * Angular step between two subZones.
-	 * Basicaly 2PI/subs, but in case of half circle (borders) it's PI/subs!
-	 * Evaluated during init.
-	 */
-	_m_stp,
-
-	/*
-	 * Creates a BagZone with its subZones.
-	 * Its size and position should be initialized by setting the "_SCALE" (float) and "_VERTICES" (Point[1]) properties;
-	 * @param subs	A subZone table whose parent is this.
-	 */
-	Constr;
+JMI.script.BagZone = (function() {
 	
-	Constr = function( subs) {
-		m_subZones  = subs;
-	}
-	Constr.prototype = {
-		constructor: com.socialcomputing.jmi.script.BagZone,
-		version: "2.0"
-	}
-	return Constr;
+	var BagZone = function(subZones) {
+	    // Clusterized subZones table.
+	    //:Array = null;
+        this._subZones = []; 
+    
+        /*
+         * Initial angular direction of Satellites.
+         * It change depending on the distance to window borders.
+         * The main zone will start at this angle. Evaluated during init.
+         */
+        this._dir = null;
+    
+        /*
+         * Angular step between two subZones.
+         * Basicaly 2PI/subs, but in case of half circle (borders) it's PI/subs!
+         * Evaluated during init.
+         */
+        this._stp = null;
+    
+        /*
+         * Creates a BagZone with its subZones.
+         * Its size and position should be initialized by setting the "_SCALE" (float) and "_VERTICES" (Point[1]) properties;
+         * @param subs  A subZone table whose parent is this.
+         */
+		this._subZones = subZones;
+	};
+	
+	BagZone.prototype = {
+		constructor: JMI.script.BagZone,
+
+        /*
+         * Perform precalc and basic initialisation.
+         * Initialize the BBox of this zone, the max BBox of all zones and the BBox of the plan.
+         * Eval the border distance and set the initial direction and side bits.
+         * It also perform basic initialisation through inheritance.
+         * 
+         * @param applet    applet owning this zone.
+         * @param s         A Sprite on which this must be painted.
+         * @param isFirst   True if this init is the first one. False if this is a "refresh" init.
+         */
+        init: function(applet, s, isFirst) {
+            var i,
+                nbSubZones = this._subZones != null ? this._subZones.length : 0;
+            // TODO super.init(applet, s, isFirst);
+            var restSwhBounds, curSwhBounds; 
+            
+            // First time init
+            if (isFirst) {
+                this._parent = null;
+        
+                if (nbSubZones > 0) this._stp = JMI.script.Base.Pi2 / (nbSubZones + 1);
+                this._dir = 10.0;
+        
+                for (i = 0 ; i < nbSubZones ; i ++) {
+                    this._subZones[i]._parent = this;
+                }
+        
+                restSwhBounds = this._restSwh.getBounds(applet, s.graphics, this, false);          
+                curSwhBounds  = this._curSwh.getBounds(applet, s.graphics, this, true);
+                this._bounds = restSwhBounds.union(curSwhBounds);
+                
+                var isLeft = this._bounds.x < 0;
+        
+                if (nbSubZones > 0) {
+                    // float dir = 0.f,
+                    var stp = 0.25 * Base.Pi2;
+                    // isLeft || isRight
+                    if (isLeft || (this._bounds.x + this._bounds.width > applet.width)) {
+                        // TODO : portage, regarder si le décalage de bit fonctionne avec le même opérateur
+                        this._stp = JMI.script.Base.Pi2 / (nbSubZones << 1);
+        
+                        if (isLeft) {
+                            this._dir = -stp;
+                            this._subZones[nbSubZones - 1]._flags |= SIDE_BIT | LEFT_BIT;
+                        }
+                        else {
+                            this._dir = stp;
+                            this._subZones[nbSubZones - 1]._flags |= SIDE_BIT;
+                        }
+                    }
+                }
+                
+                // isLeft || isRight
+                if (isLeft || (this._bounds.x + this._bounds.width > applet.width)) {
+                    this._flags |= isLeft ? SIDE_BIT | LEFT_BIT : SIDE_BIT;
+                }
+            }
+        
+            restSwhBounds     = this._restSwh.getBounds(applet, s.graphics, this, false);
+            var win           = applet.plan._prevBox.union(restSwhBounds);
+            curSwhBounds      = this._curSwh.getBounds(applet, s.graphics, this, true);
+            this._bounds     = restSwhBounds.union(curSwhBounds);
+            
+            /*
+            this.m_bounds = this.m_restSwh.getBounds(applet, s.graphics, this, false);
+            var win:Rectangle = applet.plan.m_prevBox.union(m_bounds);
+            this.m_bounds = this.m_bounds.union(m_curSwh.getBounds(applet, s.graphics, this, true));
+            */
+            
+            if (win.y > this._bounds.y) {
+                win.height += win.y - this._bounds.y;
+                win.y      = this._bounds.y;
+            }
+            else if (win.y + win.height < this._bounds.y + this._bounds.height) {
+                win.height  = this._bounds.y + this._bounds.height - win.y;
+            }
+        
+            applet.plan._prevBox = win;
+            this._bounds.inflate(2, 2);
+        
+            var w = this._bounds.width;
+            var h = this._bounds.height;
+            var maxBox = applet.plan._maxBox;
+        
+            if (w > maxBox.width)  maxBox.width  = w;
+            if (h > maxBox.height) maxBox.height = h;
+        
+            this._bounds = this._bounds.intersection(applet.size.toRectangle());
+        },
+        
+        /**
+         * Paint this Place when the cursor hover it.
+         * This is achieved by blitting the basic background and drawing the cur swatch over it.
+         * Those operation are made in another buffer that is finaly blitted on the Applet's Graphics.
+         * This reduce CPU overhead and avoid screen flickering.
+         * 
+         * @param applet    applet owning this zone.
+         */
+        paintCur: function(applet) {
+            // Copy backDrawingSurface hovered zone to curDrawingSurface
+            // Use this method instead of ImageUtil.copy to improve performance  
+            var backBitmap = new BitmapData(this._bounds.width + this._bounds.x, this._bounds.height + this._bounds.y);
+            backBitmap.draw(applet.backDrawingSurface, null, null, null, 
+                            new JMI.script.Rectangle(this._bounds.x, this._bounds.y, 
+                                                     this._bounds.width + this._bounds.x, this._bounds.height + this._bounds.y));
+            applet.curDrawingSurface.graphics.beginBitmapFill(backBitmap);
+            applet.curDrawingSurface.graphics.drawRect(this._bounds.x, this._bounds.y, this._bounds.width, this._bounds.height);
+            applet.curDrawingSurface.graphics.endFill();
+            
+            this._curSwh.paint(applet, applet.curDrawingSurface, this, true, true, JMI.script.Satellite.ALL_TYP, true);
+            /*
+                bufGfx.translate( m_bounds.x, m_bounds.y );
+                g.setClip( m_bounds.x, m_bounds.y, m_bounds.width, m_bounds.height );
+                g.drawImage( applet.m_plan.m_blitBuf, m_bounds.x, m_bounds.y, null );
+            */
+            applet.renderShape(applet.curDrawingSurface, this._bounds.width, this._bounds.height, 
+                               new JMI.script.Point(this._bounds.x, this._bounds.y));
+        }
+
+	};
+	
+	return BagZone;
 }());
-
-/*
- * Perform precalc and basic initialisation.
- * Initialize the BBox of this zone, the max BBox of all zones and the BBox of the plan.
- * Eval the border distance and set the initial direction and side bits.
- * It also perform basic initialisation through inheritance.
- * @param applet    WPSApplet owning this zone.
- * @param g         A Graphics on which this must be painted.
- * @param isFirst	True if this init is the first one. False if this is a "refresh" init.
- */
-com.socialcomputing.jmi.script.BagZone.prototype.init = function (applet, s, isFirst) {
-	var i,
-	 	nbSubZones = m_subZones != null ? m_subZones.length : 0;
-	// TODO super.init(applet, s, isFirst);
-	var restSwhBounds, curSwhBounds; 
-	
-	// First time init
-	if (isFirst) {
-		this.m_parent = null;
-
-		if (nbSubZones > 0) this.m_stp = Base.Pi2 / (nbSubZones + 1);
-		this.m_dir = 10.0;
-
-		for (i = 0 ; i < nbSubZones ; i ++) {
-			m_subZones[i].m_parent = this;
-		}
-
-		restSwhBounds = this.m_restSwh.getBounds(applet, s.graphics, this, false);			
-		curSwhBounds  = this.m_curSwh.getBounds(applet, s.graphics, this, true);
-		this.m_bounds = restSwhBounds.union(curSwhBounds);
-		
-		var isLeft = this.m_bounds.x < 0;
-
-		if (nbSubZones > 0) {
-			// float dir = 0.f,
-			var stp = 0.25 * Base.Pi2;
-			// isLeft || isRight
-			if (isLeft || (this.m_bounds.x + this.m_bounds.width > applet.width)) {
-				this.m_stp = Base.Pi2 / (nbSubZones << 1);
-
-				if (isLeft) {
-					m_dir = -stp;
-					m_subZones[nbSubZones - 1].m_flags |= SIDE_BIT | LEFT_BIT;
-				}
-				else {
-					m_dir = stp;
-					m_subZones[nbSubZones - 1].m_flags |= SIDE_BIT;
-				}
-			}
-		}
-		
-		// isLeft || isRight
-		if (isLeft || (this.m_bounds.x + this.m_bounds.width > applet.width)) {
-			m_flags |= isLeft ? SIDE_BIT | LEFT_BIT : SIDE_BIT;
-		}
-	}
-
-	restSwhBounds     = this.m_restSwh.getBounds(applet, s.graphics, this, false);
-	var win			  = applet.plan.m_prevBox.union(restSwhBounds);
-	curSwhBounds      = this.m_curSwh.getBounds(applet, s.graphics, this, true);
-	this.m_bounds     = restSwhBounds.union(curSwhBounds);
-	
-	/*
-	this.m_bounds = this.m_restSwh.getBounds(applet, s.graphics, this, false);
-	var win:Rectangle = applet.plan.m_prevBox.union(m_bounds);
-	this.m_bounds = this.m_bounds.union(m_curSwh.getBounds(applet, s.graphics, this, true));
-	*/
-	
-	if (win.y > this.m_bounds.y) {
-		win.height += win.y - this.m_bounds.y;
-		win.y      = this.m_bounds.y;
-	}
-	else if (win.y + win.height < this.m_bounds.y + this.m_bounds.height) {
-		win.height	= this.m_bounds.y + this.m_bounds.height - win.y;
-	}
-
-	applet.plan.m_prevBox = win;
-	this.m_bounds.inflate(2, 2);
-
-	var w = this.m_bounds.width;
-	var h = this.m_bounds.height;
-	var maxBox = applet.plan.m_maxBox;
-
-	if (w > maxBox.width)  maxBox.width  = w;
-	if (h > maxBox.height) maxBox.height = h;
-
-	this.m_bounds = this.m_bounds.intersection(applet.size.toRectangle());
-}
-
-/**
- * Paint this Place when the cursor hover it.
- * This is achieved by blitting the basic background and drawing the cur swatch over it.
- * Those operation are made in another buffer that is finaly blitted on the Applet's Graphics.
- * This reduce CPU overhead and avoid screen flickering.
- * @param applet    WPSApplet owning this zone.
- * @param g         A Graphics on which this must be painted.
- */
-com.socialcomputing.jmi.script.BagZone.prototype.paintCur = function (applet) {
-		/*
-	bufGfx.drawImage( applet.m_backImg, 0, 0, m_bounds.width, m_bounds.height, m_bounds.x, m_bounds.y, m_bounds.x + m_bounds.width, m_bounds.y + m_bounds.height, null );
-	bufGfx.translate( -m_bounds.x, -m_bounds.y );*/
-    
-    // Copy backDrawingSurface hovered zone to curDrawingSurface
-    // Use this method instead of ImageUtil.copy to improve performance  
-    var backBitmap = new BitmapData(m_bounds.width+m_bounds.x, m_bounds.height+m_bounds.y);
-    backBitmap.draw(applet.backDrawingSurface, null, null, null, new Rectangle(m_bounds.x, m_bounds.y, m_bounds.width+m_bounds.x, m_bounds.height+m_bounds.y));
-    applet.curDrawingSurface.graphics.beginBitmapFill(backBitmap);
-    applet.curDrawingSurface.graphics.drawRect(m_bounds.x, m_bounds.y, m_bounds.width, m_bounds.height);
-    applet.curDrawingSurface.graphics.endFill();
-    
-	m_curSwh.paint( applet, applet.curDrawingSurface, this, true, true, Satellite.ALL_TYP, true);
-	/*
-		bufGfx.translate( m_bounds.x, m_bounds.y );
-		g.setClip( m_bounds.x, m_bounds.y, m_bounds.width, m_bounds.height );
-		g.drawImage( applet.m_plan.m_blitBuf, m_bounds.x, m_bounds.y, null );
-*/
-		applet.renderShape( applet.curDrawingSurface, m_bounds.width, m_bounds.height, new com.socialcomputing.jmi.script.Point(m_bounds.x, m_bounds.y));
-}
-
-com.socialcomputing.jmi.script.BagZone.prototype.get_m_dir = function () {
-    return _m_dir;
-}
-
-com.socialcomputing.jmi.script.BagZone.prototype.set_m_dir = function (value) {
-    _m_dir = value;
-}
-
-com.socialcomputing.jmi.script.BagZone.prototype.get_m_stp = function () {
-    return _m_stp;
-}
-
-com.socialcomputing.jmi.script.BagZone.prototype.set_m_stp = function (value) {
-	_m_stp = value;
-}
