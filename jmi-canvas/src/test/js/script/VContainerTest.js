944509b0c38a196be8aeb47c42f434ce0af208e8
Jonathan Dray <jonthan.dray@gmail.com>
2012-01-15 23:07:27 +0100

    Mise à jour du test unitaire de VContainer pour supprimer les _

--- jmi-canvas/src/test/js/script/VContainerTest.js
+++ jmi-canvas/src/test/js/script/VContainerTest.js
@@ -6,10 +6,10 @@ TestCase("VcontainerTestCase", {
         var vContainer1 = new JMI.script.VContainer(25, false);
         var vContainer2 = new JMI.script.VContainer("hello");
         
-        assertEquals(25, vContainer1._value);
-        assertEquals(false, vContainer1.isBound());
+        assertEquals(25, vContainer1.value);
+        assertEquals(false, vContainer1.isBound);
      
-        assertEquals("hello", vContainer2._value);
-        assertEquals(false, vContainer2.isBound());
+        assertEquals("hello", vContainer2.value);
+        assertEquals(false, vContainer2.isBound);
     }
 });
