de8487137aff8132eebfdc75f0417c4373294ff0
Jonathan Dray <jonthan.dray@gmail.com>
2012-01-16 18:43:53 +0100

    Modification de la class Dimension :   * Le constructeur peut maintenant prendre un rectangle (à la place de la     méthode publique statique)   * Constructeur sans paramètre = Dimensions avec largeur et hauteur = 0
    Ajout des tests unitaires pour Dimension.

--- jmi-canvas/src/main/js/script/Dimension.js
+++ jmi-canvas/src/main/js/script/Dimension.js
@@ -2,38 +2,35 @@ JMI.namespace("script.Dimension");
 
 JMI.script.Dimension = (function() {
 	
-	var Dimension = function(w, h) {
-		this.width = w;
-		this.height = h;
+	var Dimension = function(width, height) {
+	    // If no arguments are given in the constructor call, initialize the instance with 0 values;
+	    if(arguments.length == 0) {
+            width = 0;
+            height = 0;
+        }
+        // if 1 argument is given : assume it is & rectangle to copy values from
+        if(arguments.length == 1 && width instanceof JMI.script.Rectangle) {
+            height = width.height;
+            width  = width.width;
+        }
+		this.width  = width;
+		this.height = height;
 	};
 	
     Dimension.prototype = {
         constructor: JMI.script.Dimension,
 		
 		resize: function(d) {
-			return new JMI.script.Dimension(Math.max(this.width, d._width), Math.max(this.height, d._height));
+			return new JMI.script.Dimension(Math.max(this.width, d.width), Math.max(this.height, d.height));
 		},
 		
 		/**
          * Create a <code>Rectangle</code> instance with height and width of the current Dimension
          */
 		toRectangle: function(){
-            return new JMI.script.Rectangle(0, 0, this.width, this.height);
+            return new JMI.script.Rectangle(this.width, this.height);
         }
 	};
 	
 	return Dimension;
 }());
-
-
-// Public static methods
-/**
- *  Construct a new Dimension Object from a <code>Rectangle</code>
- * 	It is done this way instead of a specific constructor because of ActionScript3
- * contructor overload limitations : on class can only have one constructor signature.
- *  
- */
-// TODO : Try to handle this with the class constructor
-JMI.script.Dimension.fromRectangle = function(rect) {
-	return new JMI.script.Dimension(rect.width, rect.height);
-};
